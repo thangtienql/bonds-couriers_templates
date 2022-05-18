86335c9f540330adf885b49595b8605ad45f101f
thangtienql <thangtienql@gmail.com>
2022-05-18 17:17:46 +0700

    update 18-5

--- assets/js/bonds-common.js
+++ assets/js/bonds-common.js
@@ -7,6 +7,8 @@ $(document).ready( function() {
 
     sortByNameOfTable();
     limitCharacterTextArea();
+    requireValueForInput();
+    placeHolderInputDate();
 });
 
 function booking_table(){
@@ -60,24 +62,120 @@ function sortByNameOfTable(){
 
 function limitCharacterTextArea(){
 
-    $('.limit_character').keyup( function() {
+    $('.limit_line').each( function() {
 
-        var text = $(this).val().replace("\n", "");
+        $(this).keyup( function() {
 
-        if (text.length > 20) {
+            var text = $(this).val().replace("\n", ""),
+                $count_rows = $(this).val().split("\n").length,
+                $val_character = $(this).val();
+                
 
-            var chunks = text.match(/.{1,20}/g);
+            if (text.length > 20) {
 
-            if (chunks.length > 20) { 
+                var chunks = text.match(/.{1,20}/g);
 
-                chunks.splice(20, chunks.length - 1);
+                if (chunks.length > 20) { 
+                    chunks.splice(20, chunks.length - 1);
+                }
 
-                alert("if text have more than 10 lines is removed");
-
+                $(this).val( chunks.join("\n") );
             }
 
-            $(this).val( chunks.join("\n") );
+            if( $val_character == '' ){
+                $(this).siblings('.text-status').find('span').text(0);
+            }else{
+                $(this).siblings('.text-status').find('span').text( $count_rows );
+            }
+            
+        });
+    });
+
+    $('.limit_character').each( function() {
+        $(this).keyup( function() {
+            var $limit_character = 300,
+                $current_character = $(this).val().length,
+                $character_remaining = $limit_character - $current_character;
+            
+            $(this).siblings('.text-status').find('span').text( $character_remaining );
+        });
+    });
+
+}
+
+function placeHolderInputDate(){
+    $('input[type=time]').each( function() {
+        $(this).on({
+            blur(){
+                if( $(this).val() ){
+                    $(this).addClass('__has_value');
+                }else{
+                    $(this).removeClass('__has_value');
+                }
+            }
+        });
+    });
+    
+    $('input[name=delivery_time_last_a2]').on({
+        blur(){
+            if( $(this).val() ){
+                $('input[name=delivery_window_from_a2]').attr('disabled', 'disabled');
+                $('input[name=delivery_window_to_a2]').attr('disabled', 'disabled');
+
+                $('input[name=delivery_window_to_a2]').parents('.bonds-field').addClass('disable');
+            }else{
+                $('input[name=delivery_window_from_a2]').removeAttr('disabled');
+                $('input[name=delivery_window_to_a2]').removeAttr('disabled');
+
+                $('input[name=delivery_window_to_a2]').parents('.bonds-field').removeClass('disable');
+            }
         }
+    });
+
+    $('input[name=delivery_window_from_a2], input[name=delivery_window_to_a2]').on({
+        blur(){
+            if( $(this).val() ){
+                $('input[name=delivery_time_last_a2]').attr('disabled', 'disabled');
+
+                $('input[name=delivery_time_last_a2]').parents('.bonds-field').addClass('disable');
+            }else{
+                $('input[name=delivery_time_last_a2]').removeAttr('disabled');
+
+                $('input[name=delivery_time_last_a2]').parents('.bonds-field').removeClass('disable');
+            }
+        }
+    });
+
+}
+
+function requireValueForInput(){
+    $('input.require').each( function() {
+        var $label_field = $(this).prev().text(),
+            $elm_note    = $('<p class="validate">you must enter a value for ' + $label_field +'</p>') ;
+
+        $(this).keyup( function() {
+
+            if( $(this).val() == '' ){
+                $(this).parents('.bonds-field').addClass('warning');
+                $(this).parents('.bonds-field').append($elm_note);
+            }else{
+                $(this).parents('.bonds-field').removeClass('warning');
+                $(this).parents('.bonds-field').find('.validate').remove();
+            }
+        });
+
+        $(this).on({
+            blur(e){
+                if( $(this).val() == '' ){
+                    $(this).parents('.bonds-field').addClass('warning');
+                    $(this).parents('.bonds-field').append($elm_note);
+                }else{
+                    $(this).parents('.bonds-field').removeClass('warning');
+                    $(this).parents('.bonds-field').find('.validate').remove();
+                }
+            }
+        });
+
     });
 }
 
--- assets/js/step-common.js
+++ assets/js/step-common.js
@@ -17,6 +17,10 @@ $( document ).ready(function() {
                
                var $btnPrev =  $form.find('.st-prev');
 
+               var $btnCancel = $form.find('.st-cancel');
+
+               var $btnSubmit = $form.find('.btn-submit-form');
+
                var $btnRedirect = $form.find('.cta-button.redirect');
                $btnRedirect.hide();
 
@@ -34,15 +38,17 @@ $( document ).ready(function() {
 
                      next_current++;
                      prev_current++;
+                     
                      //Next
                      if(next_current >= $totalStep ){
-                      //  $btnNext.css('visibility','hidden');
                        $btnNext.hide();
+                       $btnPrev.hide();
                        $btnRedirect.show();
 
+                       $btnCancel.show();
+                       $btnSubmit.show();
+
                      }else{
-                      //  $btnPrev.css('visibility','visible');
-                      //  $btnNext.css('visibility','visible');
                       $btnPrev.show();
                       $btnNext.show();
                      }
@@ -59,14 +65,10 @@ $( document ).ready(function() {
                      prev_current--;
 
                      if(prev_current <= 0 ){
-                      //  $btnPrev.css('visibility','hidden');
                       $btnPrev.hide();
-                      //  $btnNext.css('visibility','visible');
                       $btnNext.show();
                      }else{
-                      //  $btnPrev.css('visibility','visible');
                       $btnPrev.show();
-                      //  $btnNext.css('visibility','visible');
                       $btnNext.show();
                      }
 
@@ -82,14 +84,10 @@ $( document ).ready(function() {
                  }else{
                    if($totalStep == 0) return;
                    if($totalStep == 1) {
-                    //  $btnPrev.css('visibility','hidden');
                     $btnPrev.hide();
-                    //  $btnNext.css('visibility','hidden');
                     $btnNext.hide();
                    }else{
-                    //  $btnPrev.css('visibility','hidden');
                     $btnPrev.hide();
-                    //  $btnNext.css('visibility','visible');
                     $btnNext.show();
                    }
                  }
@@ -106,12 +104,142 @@ $( document ).ready(function() {
                  updateStep('prev');
                });
 
-               var $eClaims = $form.find('.rt-template-add-new-elaims');
+               var $eClaims = $form.find('.bonds-template-add-new-booking');
                if($eClaims.length > 0){
                  renderFunctionseClaims($eClaims);
                }
 
            }
+
+        }
+
+        const renderFunctionseClaims = function($eClaims){
+            var $fields = $eClaims.find('.bonds-fields-bookings');
+            var $addNew = $eClaims.find('.bonds-add-another-leg');
+            // var $clearForm = $eClaims.find('.rt-clear-form');
+            var $newline = $fields.find('.bonds-item-booking[data-field="0"]').html();
+            var $countLine = 1;
+
+            if ($countLine == 1) {
+              $eClaims.find('.service-level').remove();
+            }
+
+            const htmlNewLine = function (numLine){
+              return '<div class="bonds-item-booking new-line" data-field="'+numLine+'">'+$newline.replaceAll('[0]', '['+numLine+']')+'</div>';
+            }
+
+            const renderFuncSelects = function(){
+              $('.custom-select-wrap').each(function () {
+                  // Cache the number of options
+                  var $this = $(this);
+
+                  var isNewLine = $this.closest('.bonds-item-booking');
+
+                  var $select = $this.find('select');
+
+                  if(isNewLine.hasClass('new-line') && !$this.hasClass('added')){
+                    var $custom_select = $this.find('.custom-select') // Show the first select option in the styled div
+                    $this.addClass('added');
+
+                    // Insert an unordered list after the styled div and also cache the list
+                    let $list = $this.find('.options');
+
+                    // Cache the list items
+                    let $listItems = $list.children('li');
+
+                    // Show the unordered list when the styled div is clicked (also hides it if the div is clicked again)
+                    $custom_select.click(function (e) {
+                       e.stopPropagation();
+                       if(!$(this).hasClass('active')){
+                         $('div.custom-select.active').each(function () {
+                             $(this).removeClass('active').next('ul.options').hide();
+                         });
+                       }
+                       $(this).toggleClass('active').next('ul.options').toggle();
+                    });
+                    // Hides the unordered list when a list item is clicked and updates the styled div to show the selected list item
+                    // Updates the select element to have the value of the equivalent option
+                    $listItems.click(function (e) {
+                       e.stopPropagation();
+                       $custom_select.text($(this).text()).removeClass('active');
+                       $select.val($(this).attr('rel')).change();
+                       $list.children('li').removeClass('selected');
+                       $(this).addClass('selected');
+                       $list.hide();
+                       /* alert($this.val()); Uncomment this for demonstration! */
+                    });
+                    // Hides the unordered list when clicking outside of it
+                    $(document).click(function () {
+                       $custom_select.removeClass('active');
+                       $list.hide();
+                    });
+                  }
+              });
+            }
+
+            const renderFuncTextArea = function() {
+              $('.limit_line').each(function () {
+                  // Cache the number of options
+                  var $this = $(this);
+                  var isNewLine = $this.closest('.bonds-item-booking');
+
+                  if( isNewLine.hasClass('new-line') ){
+
+                    $(this).keyup( function() {
+
+                      var text = $(this).val().replace("\n", ""),
+                          $count_rows = $(this).val().split("\n").length,
+                          $val_character = $(this).val();
+                          
+
+                      if (text.length > 20) {
+
+                          var chunks = text.match(/.{1,20}/g);
+
+                          if (chunks.length > 20) { 
+                              chunks.splice(20, chunks.length - 1);
+                          }
+
+                          $(this).val( chunks.join("\n") );
+                      }
+
+                      if( $val_character == '' ){
+                          $(this).siblings('.text-status').find('span').text(0);
+                      }else{
+                          $(this).siblings('.text-status').find('span').text( $count_rows );
+                      }
+                      
+                    });
+                  }
+
+              });
+            }
+
+            //Add new line
+            $addNew.on('click',function(){
+                var $htmlNewLine = htmlNewLine($countLine);
+                $fields.append($htmlNewLine);
+                $countLine++;
+                renderFuncSelects();
+                renderFuncTextArea();
+            });
+
+            //Clear form
+            // $clearForm.on('click',function(){
+            //     let text = "Are you sure to want 'CLEAR FORM'!";
+            //     if (confirm(text) == true) {
+            //       var $htmlNewLine = htmlNewLine(0);
+            //       $fields.empty();
+            //       $fields.append($htmlNewLine);
+            //       $countLine = 1;
+            //       renderFuncSelects();
+            //       if(next_current == 4){
+            //         setTimeout(function(){
+            //           updateDataTableClaim($eClaims);
+            //         },100);
+            //       }
+            //     }
+            // });
 
         }
 
